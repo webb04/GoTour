989623ba7a7d7e34103ec764b4c3be7e9ee66b7f
webb04 <jamie.webb@warwick.ac.uk>
2017-12-29 19:35:46 +0000

    remove quit channel

--- binary-trees.go
+++ binary-trees.go
@@ -9,24 +9,18 @@ import (
 
 // Walk walks the tree t sending all values
 // from the tree to the channel ch.
-func Walk(t *tree.Tree, ch chan int, quit chan int) {
+func Walk(t *tree.Tree, ch chan int) {
 	if t.Value != 0 {
 		ch <- t.Value
 	}
 
-	if (t.Left == nil && t.Right == nil) {
-		// quit <- 1
-	}
-
 	if t.Left != nil {
-		go Walk(t.Left, ch, quit)
+		go Walk(t.Left, ch)
 	}
 
 	if t.Right != nil {
-		go Walk(t.Right, ch, quit)
+		go Walk(t.Right, ch)
 	}
-
-	// fmt.Println(reflect.TypeOf(t.Left))
 }
 
 // Same determines whether the trees
@@ -34,27 +28,23 @@ func Walk(t *tree.Tree, ch chan int, quit chan int) {
 func Same(t1, t2 *tree.Tree) bool {
 	ch1 := make(chan int, 10)
 	ch2 := make(chan int, 10)
-	quit := make(chan int, 1)
 
-	go Walk(t1, ch1, quit)
-	go Walk(t2, ch2, quit)
+	go Walk(t1, ch1)
+	go Walk(t2, ch2)
 
 	var s1 []int
 	var s2 []int
 
 	for i := 0; i < 20; i++ {
-    	select {
-        case msg1 := <-ch1:
-            fmt.Println("ch1 received", msg1)
-			      s1 = append(s1, msg1)
-		    case msg2 := <-ch2:
-            fmt.Println("ch2 received", msg2)
-			      s2 = append(s2, msg2)
-        case msg3 := <-quit:
-            fmt.Println("quit received", msg3)
-			      // return false;
-        }
+  	select {
+    	case msg1 := <-ch1:
+      	fmt.Println("ch1 received", msg1)
+			  s1 = append(s1, msg1)
+		  case msg2 := <-ch2:
+      	fmt.Println("ch2 received", msg2)
+			  s2 = append(s2, msg2)
     }
+  }
 
 	sort.Ints(s1)
 	sort.Ints(s2)
@@ -63,6 +53,7 @@ func Same(t1, t2 *tree.Tree) bool {
 }
 
 func main() {
+	// https://tour.golang.org/concurrency/8
 	fmt.Println(Same(tree.New(1), tree.New(1)))
 	fmt.Println(Same(tree.New(1), tree.New(2)))
 }
